8d241b2c84cf3b42faf2dbe7872ec044df184fba
Guillermodiazvg5 <guillermodiazvg5@hotmail.com>
2024-04-18 15:15:55 -0500

    cambio a idioma español

--- app/root.tsx
+++ app/root.tsx
@@ -67,7 +67,7 @@ export default function App({ children }: { children: React.ReactNode }) {
   useChangeLanguage(locale);
 
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
